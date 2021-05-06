75e8aced888d80949a661348585f145f0005567b
David Ranum <dlranum@us.ibm.com>
2021-05-06 15:45:28 -0500

    First commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='hellopip',
+    version='0.0.1',
+    description='My test from git',
+    url='git@github.com:ranum/hellopip.git',
+    author='davidranum',
+    author_email='ranum.david@gmail.com',
+    license='unlicense',
+    packages=['hellopip'],
+    zip_safe=False
+)
